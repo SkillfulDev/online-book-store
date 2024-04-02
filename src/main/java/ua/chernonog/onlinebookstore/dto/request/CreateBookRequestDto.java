841f9ad758dd4b44d160f857cea1a8aa3cf8c55f
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-04-02 13:16:42 +0300

    removed_id_from_CreateBookRequestDto

--- src/main/java/ua/chernonog/onlinebookstore/dto/request/CreateBookRequestDto.java
+++ src/main/java/ua/chernonog/onlinebookstore/dto/request/CreateBookRequestDto.java
@@ -1,14 +1,22 @@
 package ua.chernonog.onlinebookstore.dto.request;
 
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class CreateBookRequestDto {
-    private Long id;
+    @NotEmpty
     private String title;
+    @NotEmpty
     private String author;
+    @NotEmpty
     private String isbn;
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
