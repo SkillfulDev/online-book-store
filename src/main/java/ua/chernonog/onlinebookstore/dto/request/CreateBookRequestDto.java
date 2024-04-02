ef05d7d01d747ef6a3e2a9e9b666ed7072143b1a
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-04-02 13:37:46 +0300

    added_validation_in_CreateBookRequestDto

--- src/main/java/ua/chernonog/onlinebookstore/dto/request/CreateBookRequestDto.java
+++ src/main/java/ua/chernonog/onlinebookstore/dto/request/CreateBookRequestDto.java
@@ -1,20 +1,30 @@
 package ua.chernonog.onlinebookstore.dto.request;
 
-import java.math.BigDecimal;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateBookRequestDto {
-    @NotEmpty
+    private Long id;
+    @NotEmpty(message = "Title cannot be empty")
     private String title;
-    @NotEmpty
+    @NotEmpty(message = "Author cannot be empty")
     private String author;
-    @NotEmpty
+    @NotEmpty(message = "ISBN cannot be empty")
+    @Pattern(regexp = "^(97(8|9))?\\d{9}(\\d|X)$", message = "Invalid ISBN format")
     private String isbn;
-    @Min(0)
+    @NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.00", inclusive = false, message = "Price must be greater than 0")
     private BigDecimal price;
+    @NotEmpty
+    @Size(max = 100, message = "Description length must be less than 100 characters")
     private String description;
+    @URL(message = "Cover image must be a valid URL")
     private String coverImage;
 }
