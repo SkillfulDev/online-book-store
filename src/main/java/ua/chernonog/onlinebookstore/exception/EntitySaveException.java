35d556f5b2ed24fb9f528d220e9d8b33bf5222f5
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-04-04 17:49:13 +0300

    fixed_after_mentor_review

--- src/main/java/ua/chernonog/onlinebookstore/exception/EntitySaveException.java
+++ src/main/java/ua/chernonog/onlinebookstore/exception/EntitySaveException.java
@@ -1,8 +1,8 @@
 package ua.chernonog.onlinebookstore.exception;
 
 public class EntitySaveException extends RuntimeException {
-    public EntitySaveException(String message) {
-        super(message);
+    public EntitySaveException(String message, Throwable cause) {
+        super(message, cause);
     }
 
     public EntitySaveException(String message, Throwable cause) {
