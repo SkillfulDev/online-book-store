022ec4c691aaf07a92d222fb0dcfb2d64af9e60d
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-04-05 16:49:58 +0300

    resolved conflict

--- src/main/java/ua/chernonog/onlinebookstore/exception/EntitySaveException.java
+++ src/main/java/ua/chernonog/onlinebookstore/exception/EntitySaveException.java
@@ -4,4 +4,8 @@ public class EntitySaveException extends RuntimeException {
     public EntitySaveException(String message) {
         super(message);
     }
+
+    public EntitySaveException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
