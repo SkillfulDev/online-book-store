c20b0e403b4d0d8b3d6740f3b9fdf94980b2f463
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-04-16 13:05:36 +0300

    added categories into book create changelog for books_categories relationship

--- src/main/java/ua/chernonog/onlinebookstore/entity/Book.java
+++ src/main/java/ua/chernonog/onlinebookstore/entity/Book.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.HashSet;
